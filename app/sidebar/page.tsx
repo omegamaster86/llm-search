b55289d4033d8e10be28eeb0d55693a21a926724
omegamaster86 <masya868686@gmail.com>
2025-04-20 16:26:23 +0900

    サイドバーにロゴを追加し、新しいチャンネル作成ボタンとボーダーコンポーネントを配置

--- app/sidebar/page.tsx
+++ app/sidebar/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Border from '../components/border'
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='px-4'>
+      <div>rogo</div>
+      <button className='bg-pink-700 text-white px-4 py-2 rounded-md my-6'>新しいチャンネルを作成</button>
+      <Border />
+    </div>
   )
 }
 
