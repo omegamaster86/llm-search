f6c54df107d0b9deb9492eb6325c0cd33b78422f
omegamaster86 <masya868686@gmail.com>
2025-04-20 16:26:03 +0900

    ホームページのサイドバーの背景色を赤から青に変更し、ダッシュボードのスタイルを調整

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Sidebar from "./sidebar/page";
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <div className="w-1/5 bg-red-500">
+      <div className="w-1/5 bg-blue-700">
         <Sidebar />
       </div>
-      <div className="w-4/5 bg-blue-500">
+      <div className="w-4/5">
         <Dashboard />
       </div>
     </div>
