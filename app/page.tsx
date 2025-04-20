7283bd5b305b199882c8073587ad1c13a8395216
omegamaster86 <masya868686@gmail.com>
2025-04-20 15:46:57 +0900

    ダッシュボードとサイドバーコンポーネントを追加し、ホームページのレイアウトを更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-
+import Dashboard from "./dashboard/page";
+import Sidebar from "./sidebar/page";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex h-screen">
+      <div className="w-1/5 bg-red-500">
+        <Sidebar />
+      </div>
+      <div className="w-4/5 bg-blue-500">
+        <Dashboard />
+      </div>
     </div>
   );
 }
